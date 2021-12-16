79a6f158e661523658266bf0786a123b42123d18
caleb <caled@nhnedu.com>
2021-12-16 10:35:25 +0900

    :construction: work on watch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,67 @@
 <template>
     <div>
-        <h1>Welcome to </h1>
+        <h1>Welcome to {{title2}}</h1>
+        <input type="text" v-model="input1" />
+        <button type="button" @click="getData">Get</button>
+        <button type="button" @click="setData">Set</button>
     </div>
 </template>
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      title: "개발자의 품격",
+      title2: "Seoul",
+      input1: "abcd",
+      options: [
+        { v: "S", t: "Seoul" },
+        { v: "J", t: "Jeju" },
+        { v: "B", t: "Busan" },
+      ],
+      region: "B",
+      tableShow: false,
+    };
+  },
+  watch: {
+    input1: function (val) {
+        console.log(val + this.input1)
+    },
+    title() {},
+  },
+  methods: {
+    getData() {
+      alert(this.input1);
+    },
+    setData() {
+      this.input1 = "12345";
+    },
+    changeRegion() {
+      alert(this.region);
+    },
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  beforeUpdate() {
+    // console.log("beforeUpdate");
+  },
+  updated() {
+    // console.log("updated");
+  },
+  beforeDestroy() {
+    console.log("beforeDestroy");
+  },
+  destroyed() {
+    console.log("beforeDestroy");
+  },
+};
 </script>
